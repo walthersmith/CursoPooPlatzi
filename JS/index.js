233c460bd4c6cea0fb2d14d523fc0a22d86e0b45
Walther Smith <wsfrancootero@gmail.com>
2021-01-08 20:48:30 -0500

    se crean las clases de Uber* para Python y JavaScript  y se realiza el ejemplode herencia en cada uno de los lenguajes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWQ123",new Account('Andres Herrera',"AWD444"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("XCV428",new Account('Andra Ferran',"AWD452"),"Chevrolet","Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
